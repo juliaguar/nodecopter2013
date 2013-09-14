a9672306f9bcb965720d8f2bd833956663fc959d
Finn <derfinn@gmail.com>
2013-09-14 10:34:06 +0200

    Wrapping up the event

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server, {log: false});
 var arDrone = require('ar-drone');
 var fs = require('fs');
 
@@ -22,6 +22,19 @@ client.config('control:altitude_max', 3000);
 
 io.sockets.on('connection', function (socket) {
   //socket.emit('drone', { hello: 'world' });
+
+  socket.on('droneController', function(data) {
+    console.log("Please: " + data.action);
+    if(data.action == "stop")  client.stop(); 
+    if(data.action == "land") { client.stop(); client.land(); }
+    if(data.action == "up")  client.up(0.5); 
+    if(data.action == "down") client.down(0.5);
+    if(data.action == "left") client.counterClockwise(0.5);
+    if(data.action == "right") client.clockwise(0.5);
+    if(data.action == "front") client.front(0.5);
+    if(data.action == "takeoff") client.takeoff();
+  });
+
   client.on('navdata', function(event) {
   if(event) {
     var data = {
@@ -31,25 +44,29 @@ io.sockets.on('connection', function (socket) {
       rotation: event.demo.rotation.leftRight
     };
     socket.emit('drone', data);
+    if(data.altitude > 2.0) {
+      console.log("Game over!");
+      client.animateLeds('blinkRed', 5, 10);
+    }
   }  
 });
 
 });
 
 
-client.takeoff();
+//client.takeoff();
 
-var pngStream = client.getPngStream();
-pngStream.on('data', function(data) {
-      fs.writeFile('/static/image.png', data, function(err) {
-        if(!err) console.log("Image saved." + altitude);
-      });
-    });
+// var pngStream = client.getPngStream();
+// pngStream.on('data', function(data) {
+//       fs.writeFile('/static/image.png', data, function(err) {
+//         if(!err) console.log("Image saved." + altitude);
+//       });
+//     });
 
-client.after(5000, function() {
-    this.stop();
-    this.land();
-  });
+// client.after(20000, function() {
+//     this.stop();
+//     this.land();
+//   });
 
 //require('ar-drone-png-stream')(client, { port: 8000 });
 
